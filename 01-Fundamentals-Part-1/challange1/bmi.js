ea8c17f3bed463ff0490191178d40a36a1708ce4
Bogumil Zebek <bogumil.zebek@nokia.com>
2022-03-15 16:05:16 +0100

    Add challange2

--- 01-Fundamentals-Part-1/challange1/bmi.js
+++ 01-Fundamentals-Part-1/challange1/bmi.js
@@ -6,11 +6,14 @@ const mark = [{weight: 78, height: 1.69},{weight: 95, height: 1.88}];
 const john = [{weight: 92, height: 1.95}, {weight: 85, height: 1.76}];
 
 for(let i=0; i<2; i++){
-    const markBmi = calculateBmi(mark[i].weight, mark[i].height);
-    const johnBmi = calculateBmi(john[i].weight, john[i].height);
-    console.log(markBmi);
-    console.log(johnBmi);
+    const markBmi = calculateBmi(mark[i].weight, mark[i].height).toFixed(2);
+    const johnBmi = calculateBmi(john[i].weight, john[i].height).toFixed(2);
     const markHigherBMI = markBmi>johnBmi;
-    console.log(`Mark has higher BMI than John:${markHigherBMI}`);
+
+    if(markHigherBMI){
+        console.log(`Mark's BMI (${markBmi}) is higher than John's (${johnBmi})!`);
+    } else {
+        console.log(`John's BMI (${johnBmi}) is higher than Mark's (${markBmi})!`);
+    }
 }
 
