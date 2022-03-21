62019700253cead55e163c74fa289fa1be900923
Bogumil Zebek <bogumil.zebek@nokia.com>
2022-03-21 14:33:21 +0100

    Data structure chellange

--- 01-Fundamentals-Part-1/challenge4/forecast.js
+++ 01-Fundamentals-Part-1/challenge4/forecast.js
@@ -10,3 +10,13 @@ function printForecast(arr) {
 
 console.log(printForecast([17, 21, 23]));
 console.log(printForecast([12, 5, -5, 0, 4]));
+
+
+
+let a,b;
+
+const obj = {a:1, b:3, fri:{open:"bla", close: 2}};
+
+({a,b, fri:{open: o, close: c}} = obj);
+
+console.log(a,b,o, c);
--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function test() {
+  const john = {
+    name: 'Bob',
+    sayHello: () => console.log(this.name),
+  };
+  john.sayHello();
+}
+
+const john = {
+  name: 'Bob',
+  sayHello: () => {
+    console.log(this);
+  },
+};
+john.sayHello();
