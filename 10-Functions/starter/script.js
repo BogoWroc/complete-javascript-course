7144b504e8336b84f3b73415b2f64e33217fecfb
Bogumil Zebek <bogumil.zebek@nokia.com>
2022-03-23 08:16:55 +0100

    Map game

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const poll = {
+    question: "What is your favourite programming language?",
+    options: ["0: JavaScript", "1: Python", "2: Rust", "3:C++"],
+    answers: new Array(4).fill(0),
+    registerNewAnswer(){
+        let answer = prompt(`What is your favourite language? ${this.options}`);
+        if(answer>=0 && answer<=4){
+            this.answers[answer] +=1;
+        }
+        console.log(this);
+    }
+}
+;
+
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll) );
+
+
+(function () {
+    const header = document.querySelector('h1');
+    header.style.color = 'red';
+
+    header.addEventListener('click', () =>  header.style.color = 'blue');
+})();
