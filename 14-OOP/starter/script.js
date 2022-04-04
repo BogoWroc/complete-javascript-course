c948f5146c849d50c84effb52a3d384d5239c780
Bogumil Zebek <bogumil.zebek@nokia.com>
2022-04-04 07:18:52 +0200

    Add tesla

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -15,8 +15,38 @@ Car.prototype.break = function (){
     console.log(`${this.name} breaks ... current speed is ${this.speed}`);
 }
 
+const EV = function (name, currentSpeed, charge) {
+    Car.call(this, name, currentSpeed);
+    this._charge = charge;
+}
+
+EV.prototype = Object.create(Car.prototype)
+
+EV.prototype.charge = function (chargeTo) {
+    this._charge = chargeTo;
+}
+
+EV.prototype.accelerate = function (){
+    this.speed +=20;
+    this._charge -=1;
+    console.log(`${this.name} going at ${this.speed} km/h, with a charge ${this._charge}%`);
+}
+
 const bmw = new Car('BMW', 120);
 const mercedes = new Car('Mercedes',95);
+const tesla = new EV('Tesla',120,50);
+
+tesla.accelerate()
+tesla.accelerate()
+tesla.accelerate()
+tesla.accelerate()
+tesla.charge(60)
+tesla.accelerate()
+tesla.accelerate()
+tesla.break()
+tesla.break()
+tesla.break()
+
 
 bmw.accelerate()
 bmw.accelerate()
@@ -41,3 +71,4 @@ mercedes.break()
 mercedes.accelerate()
 mercedes.accelerate()
 mercedes.accelerate()
+
