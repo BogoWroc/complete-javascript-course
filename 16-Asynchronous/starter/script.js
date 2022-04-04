88a39bb5288e67b51cf4b2ab6f1db8a59241fa6b
Bogumil Zebek <bogumil.zebek@nokia.com>
2022-04-04 07:47:56 +0200

    Promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -1,6 +1,17 @@
 'use strict';
 
-const btn = document.querySelector('.btn-country');
-const countriesContainer = document.querySelector('.countries');
+import fetch from "node-fetch";
+
+// const btn = document.querySelector('.btn-country');
+// const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+function whereAmI(lat, lon){
+    fetch(`https://geocode.xyz/${lat},${lon}?geoit=json`)
+        .then(res => res.json())
+        .then(res => console.log(`You are in ${res.state}, ${res.country}`))
+        .catch(err => console.log(err));
+}
+
+whereAmI(52.508,13.381);
